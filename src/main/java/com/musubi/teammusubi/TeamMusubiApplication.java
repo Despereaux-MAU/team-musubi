593728ab77071990bd98ad5946da5bf9f0abed0e
Despereaux <rlaehdwn025@naver.com>
2024-11-02 22:57:09 +0900

    Initial commit

---
+++ src/main/java/com/musubi/teammusubi/TeamMusubiApplication.java
@@ -0,0 +1,13 @@
+package com.musubi.teammusubi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMusubiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamMusubiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musubi/teammusubi/TeamMusubiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musubi.teammusubi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMusubiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
