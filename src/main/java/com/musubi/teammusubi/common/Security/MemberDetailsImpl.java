f7f0a4b44fd60f12087bfe6e075f553f48db877f
Despereaux <rlaehdwn025@naver.com>
2024-11-04 16:12:37 +0900

    Fix : 회원 탈퇴 기능 완성, Member Entity와 MemberDetailsImpl 분리 및 MemberDetailsServiceImpl 로 이름 변경
    비밀번호 대소문자 포함 영문+숫자+특수문자 최소 1글자씩 포함, 최소 8글자 구현 예정

---
+++ src/main/java/com/musubi/teammusubi/common/Security/MemberDetailsImpl.java
@@ -0,0 +1,50 @@
+package com.musubi.teammusubi.common.Security;
+
+import com.musubi.teammusubi.common.entity.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public record MemberDetailsImpl(Member member) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> member.getRole().name());
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Object getMember() {
+        return member;
+    }
+}
