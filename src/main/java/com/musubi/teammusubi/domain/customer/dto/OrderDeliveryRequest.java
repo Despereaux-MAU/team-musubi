d081fac842e623a1f4d8eb87ebf194c11c6c2634
34-43 <jehyuck3443@gmail.com>
2024-11-05 11:01:48 +0900

    Fix: Dto Setter 누락 수정

--- src/main/java/com/musubi/teammusubi/domain/customer/dto/OrderDeliveryRequest.java
+++ src/main/java/com/musubi/teammusubi/domain/customer/dto/OrderDeliveryRequest.java
@@ -3,8 +3,10 @@ package com.musubi.teammusubi.domain.customer.dto;
 import com.musubi.teammusubi.common.entity.Delivery;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class OrderDeliveryRequest {
 
