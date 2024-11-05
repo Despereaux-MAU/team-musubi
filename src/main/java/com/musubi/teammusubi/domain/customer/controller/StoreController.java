c5aa8c85ac42090bad49a0e668d2dffd77dcc90e
34-43 <jehyuck3443@gmail.com>
2024-11-05 10:58:40 +0900

    Feat: Store 3-layer 구현

--- src/main/java/com/musubi/teammusubi/domain/customer/controller/StoreController.java
+++ src/main/java/com/musubi/teammusubi/domain/customer/controller/StoreController.java
@@ -1,4 +1,40 @@
 package com.musubi.teammusubi.domain.customer.controller;
 
+import com.musubi.teammusubi.common.entity.Store;
+import com.musubi.teammusubi.domain.customer.dto.StoreResponse;
+import com.musubi.teammusubi.domain.customer.service.StoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/stores")
 public class StoreController {
+    private final StoreService storeService;
+
+    @GetMapping
+    public ResponseEntity<Object> readStores(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String search
+    ) {
+        Optional<String> optCategory = Optional.ofNullable(category);
+        Optional<String> optSearch = Optional.ofNullable(search);
+
+        List<Store> stores = storeService.readAllStores(page, size, optCategory, optSearch);
+        List<StoreResponse> res = stores.stream().map(StoreResponse::new).toList();
+
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> readStore(@PathVariable long id) {
+        Store store = storeService.readStore(id);
+        return ResponseEntity.ok(new StoreResponse(store));
+    }
 }
