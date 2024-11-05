fbc02f0340b1247dcab22309c24f346184721f9c
34-43 <jehyuck3443@gmail.com>
2024-11-05 10:57:31 +0900

    Feat: Delivery 3-layer 구현

--- src/main/java/com/musubi/teammusubi/domain/customer/repository/DeliveryRepository.java
+++ src/main/java/com/musubi/teammusubi/domain/customer/repository/DeliveryRepository.java
@@ -1,4 +1,10 @@
 package com.musubi.teammusubi.domain.customer.repository;
 
-public interface DeliveryRepository {
+import com.musubi.teammusubi.common.entity.Delivery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+    default Delivery findByIdSafe(long id) {
+        return findById(id).orElseThrow(() -> new RuntimeException("(Dummy Exception)Id not found"));
+    }
 }
--- src/main/java/com/musubi/teammusubi/domain/customer/service/DeliveryService.java
+++ src/main/java/com/musubi/teammusubi/domain/customer/service/DeliveryService.java
@@ -1,4 +1,38 @@
 package com.musubi.teammusubi.domain.customer.service;
 
+import com.musubi.teammusubi.common.entity.Delivery;
+import com.musubi.teammusubi.common.entity.Menu;
+import com.musubi.teammusubi.common.entity.Store;
+import com.musubi.teammusubi.domain.customer.repository.DeliveryRepository;
+import com.musubi.teammusubi.domain.customer.repository.MenuRepository;
+import com.musubi.teammusubi.domain.customer.repository.StoreRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class DeliveryService {
+
+    private final DeliveryRepository deliveryRepository;
+    private final StoreRepository storeRepository;
+    private final MenuRepository menuRepository;
+
+    @Transactional
+    public List<Delivery> readAllDeliveries() {
+        return deliveryRepository.findAll();
+    }
+
+    @Transactional
+    public Delivery orderDelivery(Long memberId, Long storeId, Long menuId, Delivery baseDelivery) {
+        Store store = storeRepository.findByIdSafe(storeId);
+        Menu menu = menuRepository.findByIdSafe(menuId);
+
+        Delivery delivery = Delivery.DeliveryFactory(memberId, store, menu, baseDelivery);
+        delivery = deliveryRepository.save(delivery);
+
+        return delivery;
+    }
 }
