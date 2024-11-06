568b45fdf35fd4648c2d37399d4e963b2ca61870
Despereaux <rlaehdwn025@naver.com>
2024-11-06 12:15:49 +0900

    fix : GlobalException 수정

--- src/main/java/com/musubi/teammusubi/common/entity/Member.java
+++ src/main/java/com/musubi/teammusubi/common/entity/Member.java
@@ -10,7 +10,6 @@ import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.http.HttpStatus;
 
 import java.util.regex.Pattern;
 
